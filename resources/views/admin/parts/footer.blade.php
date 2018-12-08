5889c76b94c88f2f0b0f2119be75ad86b0be83fb
Unknown <shafirahrd10@gmail.com>
2018-12-08 20:44:15 +0700

    admin dynamic asset

--- resources/views/admin/parts/footer.blade.php
+++ resources/views/admin/parts/footer.blade.php
@@ -1,21 +1,21 @@
 <!--   Core JS Files   -->
-<script src="assets/js/jquery-3.2.1.min.js" type="text/javascript"></script>
-<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="assets/js/material.min.js" type="text/javascript"></script>
+<script src="{{ asset('js/jquery-3.2.1.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/material.min.js') }}" type="text/javascript"></script>
 <!--  Charts Plugin -->
-<script src="assets/js/chartist.min.js"></script>
+<script src="{{ asset('js/chartist.min.js') }}"></script>
 <!--  Dynamic Elements plugin -->
-<script src="assets/js/arrive.min.js"></script>
+<script src="{{ asset('js/arrive.min.js') }}"></script>
 <!--  PerfectScrollbar Library -->
-<script src="assets/js/perfect-scrollbar.jquery.min.js"></script>
+<script src="{{ asset('js/perfect-scrollbar.jquery.min.js') }}"></script>
 <!--  Notifications Plugin    -->
-<script src="assets/js/bootstrap-notify.js"></script>
+<script src="{{ asset('js/bootstrap-notify.js') }}"></script>
 <!--  Google Maps Plugin    -->
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
 <!-- Material Dashboard javascript methods -->
-<script src="assets/js/material-dashboard.js?v=1.2.0"></script>
+<script src="{{ asset('js/material-dashboard.js?v=1.2.0') }}"></script>
 <!-- Material Dashboard DEMO methods, don't include it in your project! -->
-<script src="assets/js/demo.js"></script>
+<script src="{{ asset('js/demo.js') }}"></script>
 <script type="text/javascript">
     $(document).ready(function() {
 
