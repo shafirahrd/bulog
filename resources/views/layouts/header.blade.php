3e79886d98e6462d9d97db7266e680baea023939
Unknown <shafirahrd10@gmail.com>
2018-12-09 03:29:51 +0700

    assets rpk

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
 	            <div class="row">
 	                <div class="col-12 d-flex flex-wrap justify-content-between align-items-center">
 	                    <div class="site-branding d-flex align-items-center">
-	                        <a class="d-block" href="index.php" rel="home"><img class="d-block" src="images/logo.png" width="200" alt="logo"></a>
+	                        <a class="d-block" href="index.php" rel="home"><img class="d-block" src="{{ asset('images/logo.png') }}" width="200" alt="logo"></a>
 	                    </div><!-- .site-branding -->
 
 	                    <nav class="site-navigation d-flex justify-content-end align-items-center">
